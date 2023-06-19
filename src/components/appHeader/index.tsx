1b93fcac8e84b010afb466237d8ff57639043965
ogechukwujane <ogeonyejekwejane@gmail.com>
2023-06-19 12:23:23 +0100

    header component

--- src/components/appHeader/index.tsx
+++ src/components/appHeader/index.tsx
@@ -1,21 +1,39 @@
 import React from "react";
 import { CustomButton } from "..";
+import { headerLinks } from "../../constants";
+import hacker_rank_logo from "../../assets/hackerrank_logo-Pride.gif";
 
 export const AppHeader = () => {
   return (
-    <div className="flex justify-between">
-      <div className="">Logo</div>
-      <div>Links</div>
-      <div className="flex items-center gap-4">
-        <div className=" px-3 border-r-2 border-gray-300">
-          <p className="">For Candidates</p>
+    <div className="flex justify-between items-center mx-auto max-w-[1440px]">
+      <div className="w-full mx-10 py-5 flex justify-between items-center">
+        <div className="w-[150px]">
+          <img src={hacker_rank_logo} alt="hacker_rank_logo" width={"100%"} />
         </div>
-        <div className="flex gap-2">
-          <CustomButton title="Request Demo" buttonStyle="bg-gray-100" />
-          <CustomButton
-            title="Sign Up"
-            buttonStyle="bg-green-700 font-medium text-white"
-          />
+        <div className="flex gap-10">
+          {headerLinks.map((link) => (
+            <p
+              key={link}
+              className="text-base font-medium hover:text-green-600"
+            >
+              {link}
+            </p>
+          ))}
+        </div>
+        <div className="flex items-center gap-4">
+          <div className=" px-3 border-r-2 border-gray-300">
+            <p className="hover:text-green-600 font-medium">For Candidates</p>
+          </div>
+          <div className="flex gap-2">
+            <CustomButton
+              title="Request Demo"
+              buttonStyle="bg-gray-100 font-medium"
+            />
+            <CustomButton
+              title="Sign Up"
+              buttonStyle="bg-green-700 font-medium text-white"
+            />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/constants/index.tsx
@@ -0,0 +1,1 @@
+export const headerLinks = ["Products", "Solutions", "Resources", "Pricing"];
